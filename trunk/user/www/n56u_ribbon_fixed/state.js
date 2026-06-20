0022086677f181d1e1f8e6891c023b5c757f6956
Shallies <81499972+shallies@users.noreply.github.com>
2026-06-20 16:20:43 +0800

    Simplify footer by removing revision details
    Removed footer revision information from the footer display.

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -903,11 +903,7 @@ function show_menu(L1, L2, L3) {
 function show_footer() {
 	var $j = jQuery.noConflict();
 	var footer_code = '<div align="center" class="bottom-image"></div>\n';
-	footer_code += '<div align="center" class="copyright"><#footer_copyright_desc#></div>\n';
-
-	footer_code +='<div align="center">\n';
-	footer_code +='<span>Revised by <a href="https://github.com/shallies">hjh98</a></span>\n';
-	footer_code +='</div>\n';
+	footer_code += '<div align="center" class="copyright"><#footer_copyright_desc#><br /></div>\n';
 
 	$j("#footer").html(footer_code);
 
