24842e5006fd24b2d096f610d4cac673469ae06a
Shallies <81499972+shallies@users.noreply.github.com>
2026-07-23 11:40:15 +0800

    Disable front WAN LED in defaults.c
    Disable the WAN LED by setting its value to 0.

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -1035,7 +1035,7 @@ struct nvram_pair router_defaults[] = {
 #endif
 	{ "watchdog_cpu", "1" },
 	{ "front_led_all", "1" },
-	{ "front_led_wan", "2" },
+	{ "front_led_wan", "0" }, /* WAN LED: disabled */
 	{ "front_led_lan", "2" },
 	{ "front_led_wif", "1" },
 	{ "front_led_usb", "1" },
